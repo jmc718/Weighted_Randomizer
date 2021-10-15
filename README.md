5a35bbb224c51b62ac4fa770d2e7b02f00539e3a
jmc718 <Pretzelman718@gmail.com>
2021-10-15 14:24:58 -0600

    Completed Base Build

---
+++ README.md
@@ -0,0 +1,29 @@
+# Overview
+
+Sometimes I find myself wanting to make a decision, but I don't actually want to be the one that makes the decision. That said, there are options that I like more than others so I want those to be more likely. With that in mind, I wanted to make a weighted randomizer.
+
+I'd never worked in Kotlin before so I figured this would be a good opportunity to try it out.
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running and a walkthrough of the code.  Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I didn't really use much of anything fancy in the base version of this project. All pretty much standard Kotlin.
+
+The only external libraries that I did use was Java's file and input stream to read from a file.
+
+# Useful Websites
+
+* [Official Kotlin Documentation](https://kotlinlang.org/)
+* [Coding Math: Weighted Random](https://www.youtube.com/watch?v=MGTQWV1VfWk)
+* [SO - How can I get a random number in Kotlin?](https://stackoverflow.com/questions/45685026/how-can-i-get-a-random-number-in-kotlin)
+* [SO - How to read plain text file in Kotlin?](https://stackoverflow.com/questions/55182578/how-to-read-plain-text-file-in-kotlin)
+
+
+# Future Work
+* Use a GUI framework to make the program more user friendly
+* Add additional error checking for reading in from a file
+* [Make it object oriented](https://kotlinlang.org/docs/classes.html)
+* Handle possibility of overflow errors
--- src/app.kt
+++ src/app.kt
@@ -1,38 +1,178 @@
+/******************************************************************************
+WEIGHTED RANDOMIZER
+ Makes a weighted random choice between items specified by user input via
+ file or console input.
+******************************************************************************/
 
-// https://www.youtube.com/watch?v=MGTQWV1VfWk
+import java.io.File
+import java.io.InputStream
 
+/******************************************************************************
+Function: randomize
+In:
+    choicesMap: A Map that contains pairs of a possibility and a likelihood
+    totProb: the sum of all likelihoods
+Description: Selects a random number and moves from possibility to
+ possibility until the random number is found within the range.
+Out: The possibility that coincides with a randomly generated number
+******************************************************************************/
+fun randomize(choicesMap: Map<String, Int>, totProb: Int): String {
 
-fun randomize() {
-
-
+    val rand = (1..totProb).random()
+    // our random number is where we'll start
+    var spinner = rand
+    for ((k, v) in choicesMap) {
+        // v has a certain probability "range" per se. If it's 3, anything less
+        // than or equal to 3 will be in the range.
+        // if it is in the range of v, that's the randomly picked option
+        if (spinner <= v)
+            return k
+        // if not, we're moving on to the next one by subtracting the current
+        // probability
+        else
+            spinner -= v
+        // keep doing this until we find the right one
+    }
+    return "You shouldn't be here."
 
 }
 
-fun main() {
-//    val x = 100
-//
-//    println((0..x).random())
-
-    // Let's create a map that contains our choices and the likelihood of each
+/******************************************************************************
+Function: readFromFile
+Description: This function reads each possibility and likelihood from a file
+ in the root directory called "choice.txt". Odd numbered lines will be
+ possibilities and the even numbered ones that follow each will be the
+ likelihood.
+Out: A Map that contains pairs of a possibility and a likelihood
+******************************************************************************/
+fun readFromFile(): MutableMap<String, Int> {
+    // Initialize map to add to
     val choicesMap = mutableMapOf<String,Int>()
 
+    // read in from choice.txt into a List of strings containing each line
+    val inputStream: InputStream = File("src/choice.txt").inputStream()
+    val lineList = mutableListOf<String>()
+
+    inputStream.bufferedReader().forEachLine { lineList.add(it) }
+    var odd = true
+    val stringList = mutableListOf<String>()
+    val intList = mutableListOf<Int>()
+    lineList.forEach{
+        // All the odd numbered lines will be our choices
+        if (odd) {
+            stringList.add(it)
+        }
+        // All the even numbered lines will be our probabilities
+        if (!odd) {
+            intList.add(it.toInt())
+        }
+
+        // Flip our value
+        odd = !odd
+    }
+
+    for (n in 0 until intList.size) {
+        choicesMap.put(stringList.elementAt(n), intList.elementAt(n))
+    }
+
+    println(choicesMap)
+
+    return choicesMap
+
+}
+/******************************************************************************
+Function: cins
+Out: A Map that contains pairs of a possibility and a likelihood
+Description: This is used specifically for when the user wants to populate
+ the map manually.
+******************************************************************************/
+
+fun cins(): MutableMap<String, Int> {
+
+    val choicesMap = mutableMapOf<String,Int>()
     while(true) {
         // The user will be prompted for a thing they want to choose between
         println("Enter a possibility (leave blank to finish)")
-        var input = readLine()!!.toString()
+        val input = readLine()!!.toString()
 
         // If the string is empty, we're done: let's move on.
         if (input == "")
             break
 
-        // The user is prompted for a probability
-        println("How likely should it be?")
         // We'll call this 'int put' because I love puns
-        var intput = readLine()!!.toInt()
-        // TODO: Add error checking to make sure the input is an int
+        var intput = 0
+
+        while(true) {
+            // The user is prompted for a probability
+            println("How likely should it be?")
+
+
+            try {
+                intput = readLine()!!.toInt()
+
+                if (intput < 1) {
+                    println("Number must be at least 1")
+                }
+
+                else {
+                    break
+                }
+
+            } catch (e: NumberFormatException) {
+                println("Error $e")
+                println("Invalid input. Must be an integer number.")
+            }
+
+        }
+
 
         // Put the pair in the map
         choicesMap.put(input, intput)
+    }
+    return choicesMap
+}
+
+/******************************************************************************
+Function: randomize
+In:
+    n: The number of random choices we want to generate
+    choicesMap: A Map that contains pairs of a possibility and a likelihood
+    totProb: the sum of all likelihoods
+Description: Calls Randomize() function n number of times
+ ******************************************************************************/
+fun rollLotsRands(n: Int, choicesMap: Map<String, Int>, totProb: Int) {
+    for(n in 0..n){
+        val randSel = randomize(choicesMap, totProb)
+        println("random selection $n is $randSel")
+    }
+}
+
+/******************************************************************************
+Function: main
+Description: The master function that calls all others
+ ******************************************************************************/
+fun main() {
+//    val x = 100
+//
+//    println((0..x).random())
+
+
+    val choicesMap: MutableMap<String, Int>
+
+    while(true) {
+
+        println("Enter 1 to read from a file, enter 2 to input options by hand")
+        val input = readLine()!!.toString()
+
+        if (input == "1"){
+            choicesMap = readFromFile()
+            break
+        }
+        if (input == "2") {
+            choicesMap = cins()
+            break
+        }
+
 
     }
 
@@ -44,13 +184,20 @@ fun main() {
     }
 
     // generate a random number between 1 and the total probability
-    var rand = (1..totProb).random()
+
 
     // Loop through each item and see which one has been selected
 
+    val randSel = randomize(choicesMap, totProb)
+
+    println("Your random selection is $randSel")
+
+
+
     // For testing:
-    println(totProb)
-    println(choicesMap)
+    rollLotsRands(10, choicesMap, totProb)
+//    println(totProb)
+//    println(choicesMap)
 
 
 
